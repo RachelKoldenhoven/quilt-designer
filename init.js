7bb8ea2d04e00b946dda31c75d2f4c6d51ec30db
RachelKoldenhoven <rachel@koldenfrozen.com>
2017-10-08 20:06:26 -0600

    insert chosen svg into DOM

--- init.js
+++ init.js
@@ -6,15 +6,28 @@ export default function init() {
 
 function handleFileSelect(evt) {
   let files = evt.target.files; // FileList object
+  console.log(evt.target.files);
 
-  // files is a FileList of File objects. List some properties.
+  // files is a FileList of File objects.
   let output = [];
-  for (let i = 0, f; f = files[i]; i++) {
-    output.push('<li><strong>', f.name, '</strong>', ' (', f.type || 'n/a', ') - ',
-      f.size, ' bytes, last modified: ',
-      f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
-      '</li>');
-  }
+  let block = files[0];
+  console.log(block);
+  output.push('<li><strong>', block.name, '</strong>', ' (', block.type || 'n/a', ') - ',
+    block.size, ' bytes, last modified: ',
+    block.lastModifiedDate ? block.lastModifiedDate.toLocaleDateString() : 'n/a',
+    '</li>');
+
+  let reader = new FileReader;
+  reader.onload = (function (theFile) {
+    return function (e) {
+      let svg = document.getElementById('selectedBlock');
+      svg.innerHTML += e.target.result;
+      console.log(e.target.result);
+    };
+  })(block);
+
+  reader.readAsText(block);
+
   document.querySelector('.fileList').innerHTML = '<ul>' + output.join('') + '</ul>';
 }
 
