802bf1e43135dd132aedaeeea534aa5b5f8bd810
RachelKoldenhoven <rachel@koldenfrozen.com>
2017-10-11 12:15:04 -0600

    add shapes to a group, change color by group

--- init.js
+++ init.js
@@ -3,8 +3,16 @@ export default function init() {
 
   document.getElementById('files').addEventListener('change', handleFileSelect, false);
 
-  document.getElementById('colorChoice').addEventListener('change', selectColor, false);
+  document.getElementById('1').addEventListener('change', selectColor, false);
+  document.getElementById('1').addEventListener('click', selectGroup, false);
+
+  document.getElementById('addGroup').addEventListener('click', addGroup);
 }
+
+let selectedGroup = 1;
+let color1 = '#545454';
+document.getElementById('color1').style.background = color1;
+let selectedColor = color1;
 
 function handleFileSelect(evt) {
   let files = evt.target.files; // FileList object
@@ -21,11 +29,11 @@ function handleFileSelect(evt) {
   let reader = new FileReader;
   reader.onload = (function (theFile) {
     return function (e) {
-      // function to change color of a shape on click
-      let shade = function (e) {
+      // add shapes to a group
+      let group =  (e) => {
         let node = e.target;
+        node.setAttribute('class', selectedGroup);
         node.style.fill = selectedColor;
-        console.log('node: ', node);
       };
       
       let rawBlock = e.target.result;
@@ -35,7 +43,7 @@ function handleFileSelect(evt) {
       // childNodes are [comment, <!DOCTYPE svg>, svg#Layer_1]
       console.log(parsedBlock.childNodes[2]);
       for(let i = 1; i < parsedBlock.childNodes[2].children.length; i++) {
-        parsedBlock.childNodes[2].children[i].addEventListener('click', shade);
+        parsedBlock.childNodes[2].children[i].addEventListener('click', group);
       }
       // returns a SVGDocument
 
@@ -48,10 +56,40 @@ function handleFileSelect(evt) {
 }
 
 // color selection
-let selectedColor = '#545454';
-document.getElementById('selectedColor').style.background = selectedColor;
-
 const selectColor = (event) => {
   selectedColor = '#' + event.target.value;
-  document.getElementById('selectedColor').style.background = selectedColor;
+  let shapes = document.getElementsByClassName(selectedGroup);
+  console.log(shapes);
+  for(let i = 0; i< shapes.length; i++) {
+    shapes[i].style.fill = selectedColor;
+  }
+  document.getElementById(`color${selectedGroup}`).style.background = selectedColor;
+};
+
+// add shape/color groups
+const addGroup = () => {
+  // append group info to DOM
+  // change selected group
+  selectedGroup += 1;
+  let newGroup = document.createElement('div');
+  newGroup.innerHTML =
+    `<div class="colorGroup">
+        <h1>${selectedGroup}</h1>
+        <div class="colorInput">
+            <label for="${selectedGroup}">Enter a hex color code</label>
+            <input type="text" 
+                   name="${selectedGroup}" 
+                   id="${selectedGroup}" 
+                   placeholder="545454">
+        </div>
+        <div class="selectedColor" id="color${selectedGroup}"></div>
+    </div>`;
+  document.querySelector('div.groups').appendChild(newGroup);
+  document.getElementById(`${selectedGroup}`).addEventListener('change', selectColor, false);
+};
+
+// select a group to edit
+const selectGroup = (event) => {
+  console.log("selected group: ", event.target);
+  selectedGroup = event.target.id;
 };
